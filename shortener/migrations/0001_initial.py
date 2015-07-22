c4e7073bb8686ae4355bac21841ca8351b0ff8a4
Yves-Marie Haussonne <ymh.work+github@gmail.com>
2015-07-22 17:29:35 +0200

    delete unnecessary file + add migration

---
+++ shortener/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Link',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('url', models.URLField()),
+                ('date_submitted', models.DateTimeField(auto_now_add=True)),
+                ('usage_count', models.PositiveIntegerField(default=0)),
+            ],
+        ),
+    ]
